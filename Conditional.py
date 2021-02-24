95790b194bd387e1b891939ff789d9cdbafe2057
federpmatc <61851250+federpmatc@users.noreply.github.com>
2021-02-24 14:15:16 -0600

    Update Conditional.py

--- Conditional.py
+++ Conditional.py
@@ -2,7 +2,7 @@
 # If a student has 3 or more unexecused absences he/she is dropped from class 
 # If I enter 3 I should see the message "You will be dropped from class" 
 # If I enter 0 I should see the message "You are awesome!!! Keep up the great work" 
-# If I enter 2 I should see the message "Make sure to let your instructor know if you're going to miss any classes" 
+# If I enter 1 I should see the message "Make sure to let your instructor know if you're going to miss any classes" 
 
 absences = input('how many unexecused absences? ')
 #HINT: THe next line should take into account that user entered a string...you will need to treat price as a number
@@ -11,6 +11,6 @@ if absences > 3:
 	print("You will be dropped"
 elif absences >= 1:
 print("")
-elif:
+else:
 print("")	     
 	      
