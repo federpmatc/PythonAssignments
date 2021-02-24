a02236d1e8577fa671f2d8a61138424c40ba6b03
federpmatc <61851250+federpmatc@users.noreply.github.com>
2021-02-23 18:53:20 -0600

    Update String Assignment.py

--- String Assignment.py
+++ String Assignment.py
@@ -1,6 +1,11 @@
 # ask a user to enter their first name and store it in a variable
 # ask a user to enter their last name and store it in a variable
-# print their full name
-# Make sure you have a space between first and last name
+# ask the user how many cups of coffee that they have had today
+# ask the user how many cups of coffee that they had yesterday
+#calculate the totsl number of cups of coffee by adding the 2 numbers together
+
+# print out hello followed by their full name you have drank XX cups of coffee in the last two days!!!
+
+# Make sure you have that your sentence is formatted correctly with appropriate number of spaces
 # Make sure the first letter of first name and last name is uppercase
 # Make sure the rest of the name is lowercase
