4cabe3615289b3cd3c03745e5b7c8904c5dc62d5
federpmatc <61851250+federpmatc@users.noreply.github.com>
2021-02-24 10:35:36 -0600

    Create Date.py

---
+++ Date.py
@@ -0,0 +1,13 @@
+#Import the datetime & timedelta object classes from datetime module
+#Just like PowerShell, we can acess the methods in these objects
+
+from datetime import datetime, timedelta
+
+#today is a variable.  The variable type of today is datetime
+today = datetime.now()
+
+# print today's date
+# print the date from 52 weeks ago
+
+# ask a user to enter a date
+# print the date two weeks from the date entered
